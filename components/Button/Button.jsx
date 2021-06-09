197748e5afc8264546d63af3b57be25d2086251c
Jean-Baptiste Lindner <jeanbaptistelindner@sweetpunk.com>
2021-06-09 18:01:53 +0200

    fonctionnel quiz

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import styles from './Button.module.scss'
-import Link from "next/link";
 
-export function Button({text, href}) {
+export function Button({text, href, reset}) {
     return (
-        <div
-            className={styles.button}
-        >
-            <Link
+        <div className={styles.container}>
+            {!reset &&
+            <a
                 href={href}
+                className={styles.link}
             >
                 {text}
-            </Link>
+            </a>
+            }
+            {reset &&
+            <button
+                onClick={()=> {reset(60)}}
+                className={styles.button}
+            >
+                {text}
+            </button>}
         </div>
     )
 }
--- pages/quiz.js
+++ pages/quiz.js
@@ -3,39 +3,54 @@ import styles from '../styles/Quiz.module.scss'
 import Image from "next/image";
 import {Question} from "../components/Question/Question";
 import axios from "axios";
+import {Button} from "../components/Button/Button";
 
-function Quiz() {
-    const [timer, setTimer] = useState(60);
+function Quiz({movies, actors}) {
+    const [timer, setTimer] = useState(30);
     const [score, setScore] = useState(0);
-    const [status, setStatus] = useState(true);
-    const [currentQuestion, setCurrentQuestion] = useState(null);
-    const [questions, setQuestions] = useState(null);
+    const [indexRandomMovies, setIndexRandomMovies] = useState(Math.floor(Math.random() * movies.length));
+    const [indexRandomActors, setIndexRandomActors] = useState(Math.floor(Math.random() * actors.length));
 
     useEffect(() => {
         timer > 0 && setTimeout(() => setTimer(timer - 1), 1000);
     }, [timer]);
 
-    const checkAnswer = (e, choice) => {
-        e.preventDefault()
-        // axios.get("http://localhost:8000/answer/")
-        // .then(function (response) {
-        //     const find = response.data.cast.find(item => item.id === actorId);
-        //     const result = !!find;
-        //     res.status(200).json(result);
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        // })
+    const setIndex = ()=> {
+        setIndexRandomMovies(Math.floor(Math.random() * movies.length))
+        setIndexRandomActors(Math.floor(Math.random() * actors.length))
     }
+
+    const checkAnswer = (e, choice) => {
+        e.preventDefault();
+        const actorId = actors[indexRandomActors].id;
+        const movieId = movies[indexRandomMovies].id;
+
+        axios.get('http://localhost:8000/answer/' + actorId + '/' + movieId)
+            .then(function (response) {
+                response.data === choice ? setScore(score + 1 ) : null
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        setIndex();
+    }
+    const reset = ()=> {
+        setTimer(30);
+        setScore(0)
+    }
+
     return (
         <div className={'main'}>
             {timer !== 0 && <span className={styles.timer}>Time : {timer}</span>}
-            {timer !== 0 && <Question actor="Brad Pitt" movie="Fight Club"/>}
+            {timer !== 0 && <Question actor={actors[indexRandomActors].name} movie={movies[indexRandomMovies].original_title} />}
             {timer !== 0 && <div className={styles.containerBtn}>
                 <Image src="/remove.png" width={60} height={60} alt="" className={styles.yes} onClick={(e)=>{checkAnswer(e , false)}}/>
                 <Image src="/check.png" width={60} height={60} alt="" className={styles.no} onClick={(e)=>{checkAnswer(e , true)}}/>
             </div>}
-            {timer === 0 && <div className={styles.score}>{score}</div>}
+            {timer === 0 && <h2 className={styles.over}>The quiz is over</h2>}
+            {timer === 0 && <div className={styles.score}>Your score : <span className={styles.number}>{score}</span></div>}
+            {timer === 0 && <Button text={'Retry'} reset={reset}/>}
         </div>
     )
 }
